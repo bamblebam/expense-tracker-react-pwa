d9de31467bdc95cf694261360fcd3435d8a6d241
bamblebam <burhanizrangwala@gmail.com>
2020-08-24 00:44:18 +0530

    delete and post

--- routes/api/expenses.js
+++ routes/api/expenses.js
@@ -6,4 +6,17 @@ router.get('/', (req, res) => {
     Expenses.find().sort({ date: -1 }).then(expenses => res.json(expenses))
 })
 
+router.post('/add', (req, res) => {
+    const newExpense = new Expenses({
+        name: req.body.name,
+        price: req.body.price
+    })
+    newExpense.save().then(expense => res.json(expense))
+})
+
+router.delete('/delete/:id', (req, res) => {
+    Expenses.findById(req.params.id).then(expense => expense.remove().then(() => res.json({ success: true }))).catch(err => res.status(404).json({ success: false }))
+})
+
+
 module.exports = router
