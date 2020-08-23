07a80eca3335d51d5fc27b2fe332f67e68debb57
bamblebam <burhanizrangwala@gmail.com>
2020-08-23 17:47:20 +0530

    added .env file

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyparser = require('body-parser')
+require('dotenv').config()
 
 const app = express()
 app.use(bodyparser.json())
+
+const db = process.env.mongoURI
+
+mongoose.connect(db).then(() => console.log("MongoDB connected")).catch(err => console.log(err))
+const port = process.env.port || 5000
+
+app.listen(port, () => console.log(`Server started on port: ${port}`))
