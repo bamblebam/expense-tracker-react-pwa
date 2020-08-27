390ac0b6f5bf9eb0302a08a4640affeb3e0ace0d
bamblebam <burhanizrangwala@gmail.com>
2020-08-28 03:15:30 +0530

    1

---
+++ frontend/src/components/ExpenseItem.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import uuid from 'uuid'
+
+export default class ExpenseItem extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            items=[
+                { id: uuid(), name: 'Milk', price: 45 }
+            ]
+        }
+    }
+
+    render() {
+        const { items } = this.state
+        return (
+            <Container>
+                <Button color='dark' style={{ marginBottom: '2rem' }} onClick={() => {
+                    const name = prompt('Enter Item')
+                    const price = Number(prompt('Enter Price'))
+                    if (name && price) {
+                        items: [...this.state.items, { id: uuid(), name, price }]
+                    }
+                }}>Add Item</Button>
+            </Container>
+        )
+    }
+}
