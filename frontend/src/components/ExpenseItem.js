b293ffe22a5dd96ff9f8bdbf4860794a7c38dd39
bamblebam <burhanizrangwala@gmail.com>
2020-09-07 06:00:31 +0530

    build ready

--- frontend/src/components/ExpenseItem.js
+++ frontend/src/components/ExpenseItem.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
-import uuid from 'uuid'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { getItems, deleteItem } from '../actions/itemActions.js'
