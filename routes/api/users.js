c3264bff6f204b138b7ad473f33051f049083925
bamblebam <burhanizrangwala@gmail.com>
2020-10-20 02:35:34 +0530

    auth middleware

--- routes/api/users.js
+++ routes/api/users.js
@@ -46,4 +46,37 @@ router.post('/', (req, res) => {
     })
 })
 
+router.post('/login', (req, res) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+        return res.status(400).json({ msg: "Incorrect data provided" })
+    }
+    User.findOne({ email }).then(user => {
+        if (!user) {
+            return res.status(400).json({ msg: "User does not exsist" })
+        }
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if (!isMatch) {
+                return res.status(404).json({ msg: "email or password not correct" })
+            }
+            jwt.sign(
+                { id: user.id },
+                process.env.jwtSecret,
+                { expiresIn: 3600 },
+                (err, token) => {
+                    if (err) throw err
+                    res.json({
+                        token,
+                        user: {
+                            id: user.id,
+                            username: user.username,
+                            email: user.email
+                        }
+                    })
+                }
+            )
+        })
+    })
+})
+
 module.exports = router
