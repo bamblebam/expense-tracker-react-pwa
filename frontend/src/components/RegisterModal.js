7b252652a90546a518e53bd6b4bd8072ae23587f
bamblebam <burhanizrangwala@gmail.com>
2020-10-21 09:51:27 +0530

    register done

--- frontend/src/components/RegisterModal.js
+++ frontend/src/components/RegisterModal.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { addItem } from '../actions/itemActions'
 import { register } from '../actions/authAction'
+import { clearErrors } from '../actions/errorAction'
 
 class RegisterModal extends Component {
     constructor(props) {
@@ -21,11 +22,12 @@ class RegisterModal extends Component {
     static propTypes = {
         isAuthenticated: PropTypes.bool,
         error: PropTypes.object.isRequired,
-        register: PropTypes.func.isRequired
+        register: PropTypes.func.isRequired,
+        clearErrors: PropTypes.func.isRequired,
     }
 
     componentDidUpdate(prevProps) {
-        const { error } = this.props
+        const { error, isAuthenticated } = this.props
         if (error !== prevProps.error) {
             if (error.id === 'REGISTER_FAIL') {
                 this.setState({ msg: error.msg.msg })
@@ -34,9 +36,15 @@ class RegisterModal extends Component {
                 this.setState({ msg: null })
             }
         }
+        if (this.state.modal) {
+            if (isAuthenticated) {
+                this.toggle()
+            }
+        }
     }
 
     toggle = () => {
+        this.props.clearErrors()
         this.setState({
             modal: !this.state.modal
         })
@@ -92,4 +100,4 @@ const mapStateToProps = (state) => ({
     error: state.error
 })
 
-export default connect(mapStateToProps, { register })(RegisterModal)
+export default connect(mapStateToProps, { register, clearErrors })(RegisterModal)
